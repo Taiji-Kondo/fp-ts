06cabf96d7c923a4a8ac61a83c94cb5fa801837c
Taiji Kondo <kondo@sailboat.co.jp>
2021-10-07 09:02:19 +0900

    CREATE: routing設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
+import Either from "./pages/either";
 
 function App() {
   return (
-    <div>
-      app
-    </div>
+    <BrowserRouter>
+      <h1>Functional Programing Practice</h1>
+      <Switch>
+        <Route exact path="/">
+          TOP
+        </Route>
+        <Route path="/either">
+          <Either />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
