e3f2f8036af0909a4ed7cc91311b5a5e7acf62eb
Taiji Kondo <kondo@sailboat.co.jp>
2021-10-08 13:28:26 +0900

    CREATE: 商品appleを作成

--- src/pages/either/index.tsx
+++ src/pages/either/index.tsx
@@ -1,6 +1,14 @@
 import { VFC } from "react";
+import {ProductType} from "../../types/product/ProductType";
+
+const apple: ProductType = {
+  id: 1,
+  name: 'apple',
+  price: 100
+}
 
 const Either: VFC = () => {
+  console.log(apple)
   return (
     <div>Either</div>
   )
