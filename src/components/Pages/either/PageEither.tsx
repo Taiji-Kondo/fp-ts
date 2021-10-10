42cc7b5084209fecbcde2e2deac9279e2424e7ad
Taiji-Kondo <k.taiji1108@gmail.com>
2021-10-10 11:35:56 +0900

    CREATE: 商品一覧とカート追加機能作成

--- src/components/Pages/either/PageEither.tsx
+++ src/components/Pages/either/PageEither.tsx
@@ -1,22 +1,60 @@
 import {useState, VFC} from "react";
 import {ProductType} from "../../../types/product/ProductType";
 
-const apple: ProductType = {
+const APPLE: ProductType = {
   id: 1,
   name: 'apple',
   price: 100
 }
+const ORANGE: ProductType = {
+  id: 2,
+  name: 'orange',
+  price: 80
+}
+const BANANA: ProductType = {
+  id: 1,
+  name: 'banana',
+  price: 200
+}
+
+const PRODUCTS = [APPLE, ORANGE, BANANA]
 
 const PageEither: VFC = () => {
-  const [products, setProducts] = useState<ProductType[]>()
+  const [selectedProducts, setSelectedProducts] = useState<number[]>()
 
-  const findSpecifiedCountOfProducts = ({ id }: Pick<ProductType, 'id'>): number => {
-    const specifiedProducts = products?.filter((product) => product.id === id)
-    return specifiedProducts ? specifiedProducts.length : 0
+  const findSpecifiedforSelectedProducts = (id: number): number[] => {
+    const specifiedProducts = selectedProducts?.filter((product) => product === id)
+    return specifiedProducts ?? []
+  }
+
+  const addProduct = (id: number): void => {
+    const newSelectedProducts = selectedProducts ? [...selectedProducts, id] : [id]
+    setSelectedProducts(newSelectedProducts)
   }
 
   return (
-    <div>Issuance of statement</div>
+    <>
+      <div>Issuance of statement</div>
+
+      <div>
+        <h2>Products</h2>
+        <ul>
+          {
+            PRODUCTS.map(({ id, name, price }) => {
+              return (
+                <li>
+                  <article>
+                    <h1>NAME: {name}</h1>
+                    <span>PRICE: {price}</span>
+                    <button type="button" onClick={() => addProduct(id)}>ADD</button>
+                  </article>
+                </li>
+              )
+            })
+          }
+        </ul>
+      </div>
+    </>
   )
 }
 
