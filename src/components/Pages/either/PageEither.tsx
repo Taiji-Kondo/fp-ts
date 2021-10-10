f352ce47a02002a91a21169f04baa466d540cf9a
Taiji-Kondo <k.taiji1108@gmail.com>
2021-10-10 11:53:32 +0900

    CREATE: カート表示機能作成

--- src/components/Pages/either/PageEither.tsx
+++ src/components/Pages/either/PageEither.tsx
@@ -20,15 +20,15 @@ const BANANA: ProductType = {
 const PRODUCTS = [APPLE, ORANGE, BANANA]
 
 const PageEither: VFC = () => {
-  const [selectedProducts, setSelectedProducts] = useState<number[]>()
+  const [selectedProducts, setSelectedProducts] = useState<ProductType[]>()
 
-  const findSpecifiedforSelectedProducts = (id: number): number[] => {
-    const specifiedProducts = selectedProducts?.filter((product) => product === id)
-    return specifiedProducts ?? []
-  }
+  // const findSpecifiedForSelectedProducts = (id: number): number[] => {
+  //   const specifiedProducts = selectedProducts?.filter((product) => product === id)
+  //   return specifiedProducts ?? []
+  // }
 
-  const addProduct = (id: number): void => {
-    const newSelectedProducts = selectedProducts ? [...selectedProducts, id] : [id]
+  const addProduct = (product: ProductType): void => {
+    const newSelectedProducts = selectedProducts ? [...selectedProducts, product] : [product]
     setSelectedProducts(newSelectedProducts)
   }
 
@@ -37,22 +37,42 @@ const PageEither: VFC = () => {
       <div>Issuance of statement</div>
 
       <div>
-        <h2>Products</h2>
-        <ul>
+        <h2 style={{borderBottom: '1px solid', paddingBottom: '20px', marginBottom: '0'}}>Products</h2>
+        <div style={{display: 'flex'}}>
+          <div style={{width: '80%'}}>
+            <ul style={{display: 'flex'}}>
+              {
+                PRODUCTS.map((product) => {
+                  return (
+                    <li style={{width: '33.33%'}}>
+                      <article>
+                        <h1>NAME: {product.name}</h1>
+                        <span>PRICE: {product.price}</span>
+                        <button type="button" onClick={() => addProduct(product)}>ADD</button>
+                      </article>
+                    </li>
+                  )
+                })
+              }
+            </ul>
+          </div>
           {
-            PRODUCTS.map(({ id, name, price }) => {
-              return (
-                <li>
-                  <article>
-                    <h1>NAME: {name}</h1>
-                    <span>PRICE: {price}</span>
-                    <button type="button" onClick={() => addProduct(id)}>ADD</button>
-                  </article>
-                </li>
-              )
-            })
+            selectedProducts &&
+            <div style={{width: '20%', height: '100vh', borderLeft: '1px solid'}}>
+              <ul>
+                {
+                  selectedProducts.map(({ name }) => {
+                    return (
+                      <li>
+                        <p>{name}</p>
+                      </li>
+                    )
+                  })
+                }
+              </ul>
+            </div>
           }
-        </ul>
+        </div>
       </div>
     </>
   )
