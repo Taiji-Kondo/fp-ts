dbc35e6fd589090a9d1a0c324a8a1661f8665890
Taiji-Kondo <k.taiji1108@gmail.com>
2021-10-10 12:49:31 +0900

    FIX: カート追加機能修正

--- src/components/Pages/either/PageEither.tsx
+++ src/components/Pages/either/PageEither.tsx
@@ -1,18 +1,35 @@
 import {useState, VFC} from "react";
 import {ProductType} from "../../../types/product/ProductType";
 import {PRODUCTS} from "../../../constants/Products";
+import {SelectedProductType} from "../../../types/product/SelectedProductType";
 
 const PageEither: VFC = () => {
-  const [selectedProducts, setSelectedProducts] = useState<ProductType[]>()
-
-  // const findSpecifiedForSelectedProducts = (id: number): number[] => {
-  //   const specifiedProducts = selectedProducts?.filter((product) => product === id)
-  //   return specifiedProducts ?? []
-  // }
+  const [selectedProducts, setSelectedProducts] = useState<SelectedProductType[]>()
 
   const addProduct = (product: ProductType): void => {
-    const newSelectedProducts = selectedProducts ? [...selectedProducts, product] : [product]
-    setSelectedProducts(newSelectedProducts)
+    if (selectedProducts) {
+      const specifiedProduct = getSpecifiedProduct(product)
+      if (specifiedProduct) {
+        const addCountProducts = selectedProducts.map((selectedProduct) => {
+          if (selectedProduct.id !== specifiedProduct.id) return selectedProduct
+          return {...selectedProduct, selectedCount: selectedProduct.selectedCount + 1}
+        })
+        setSelectedProducts(addCountProducts)
+      } else {
+        const addCountProduct = {...product, selectedCount: 1}
+        setSelectedProducts([...selectedProducts, addCountProduct])
+      }
+    } else {
+      const addCountProduct = {...product, selectedCount: 1}
+      setSelectedProducts([addCountProduct])
+    }
+  }
+
+  const getSpecifiedProduct = (product: ProductType): SelectedProductType | null => {
+    const selectedProduct = selectedProducts!.find(
+      (selectedProduct) => selectedProduct.id === product.id
+    )
+    return selectedProduct ?? null
   }
 
   return (
@@ -27,7 +44,7 @@ const PageEither: VFC = () => {
               {
                 PRODUCTS.map((product) => {
                   return (
-                    <li style={{width: '33.33%'}}>
+                    <li key={product.id} style={{width: '33.33%'}}>
                       <article>
                         <h1>NAME: {product.name}</h1>
                         <span>PRICE: {product.price}</span>
@@ -44,10 +61,10 @@ const PageEither: VFC = () => {
             <div style={{width: '20%', height: '100vh', borderLeft: '1px solid'}}>
               <ul>
                 {
-                  selectedProducts.map(({ name }) => {
+                  selectedProducts.map(({ id, name, selectedCount }) => {
                     return (
-                      <li>
-                        <p>{name}</p>
+                      <li key={id}>
+                        <p>{name} x{selectedCount}</p>
                       </li>
                     )
                   })
