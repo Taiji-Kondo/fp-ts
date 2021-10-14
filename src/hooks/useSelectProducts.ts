c0f733a56c2de4a695147b7349d1c2658434af67
Taiji Kondo <kondo@sailboat.co.jp>
2021-10-14 18:23:09 +0900

    REFACTOR: 該当する商品が存在するか検索するをカリー化

--- src/hooks/useSelectProducts.ts
+++ src/hooks/useSelectProducts.ts
@@ -1,6 +1,6 @@
 import {useCallback, useState} from "react";
 import {ProductType} from "../types/product/ProductType";
-import { Option, fold, fromNullable } from 'fp-ts/Option';
+import { fold, fromNullable } from 'fp-ts/Option';
 import {filter, map} from 'fp-ts/lib/Array';
 import { pipe } from 'fp-ts/function'
 
@@ -8,24 +8,36 @@ export const useSelectProducts = () => {
   const [selectedProducts, setSelectedProducts] = useState<ProductType[]>([])
 
   const addProduct = useCallback((product: ProductType): void => {
-    const specifiedProduct = _getSpecifiedProduct(product)
-    fold(() => {
-      const addCountProduct = {...product, selectedCount: 1}
-      setSelectedProducts([...selectedProducts, addCountProduct])
-    }, (specified: ProductType) => {
-      const addCountProducts = _calcSelectedCount()(specified, true)
-      setSelectedProducts(addCountProducts)
-    })(specifiedProduct)
+    setSelectedProducts(_addedSelectedCountProduct(product))
   }, [selectedProducts])
 
   const removeProduct = useCallback((product: ProductType): void => {
     setSelectedProducts(_removedSelectedCountProduct(product))
   }, [selectedProducts])
 
-  const _getSpecifiedProduct = (product: ProductType): Option<ProductType> =>
-    fromNullable(selectedProducts.find(
+  // 商品数をプラスしたい商品を引数にとって新しい商品一覧を返す
+  const _addedSelectedCountProduct = (product: ProductType) => pipe(
+    _getSpecifiedProduct()(product),
+    fold(() => {
+      const addCountProduct = {...product, selectedCount: 1}
+      return [...selectedProducts, addCountProduct]
+    }, (specified: ProductType) => {
+      return _calcSelectedCount()(specified, true)
+    })
+  )
+
+  // 商品数をマイナスしたい商品を引数にとって新しい商品一覧を返す
+  const _removedSelectedCountProduct = (product: ProductType): ProductType[] => pipe(
+    _calcSelectedCount()(product, false),
+    _removeZeroSelectedCountProduct
+  )
+
+  // 該当する商品が存在するか検索する
+  const _getSpecifiedProduct = (productions: ProductType[] = selectedProducts) => {
+    return (product: ProductType) => fromNullable(productions.find(
       (selectedProduct) => selectedProduct.id === product.id
     ))
+  }
 
   const _calcSelectedCount = (productions: ProductType[] = selectedProducts) => {
     // operator ? increment : decrement
@@ -39,12 +51,6 @@ export const useSelectProducts = () => {
       })(productions)
     }
   }
-
-  // 変更したい商品を引数にとって新しい商品一覧を返す
-  const _removedSelectedCountProduct = (product: ProductType): ProductType[] => pipe(
-    _calcSelectedCount()(product, false),
-    _removeZeroSelectedCountProduct
-  )
 
   // 商品数が0の商品を取り除いた配列を返す
   const _removeZeroSelectedCountProduct = filter<ProductType>(
