7f1d17990bc6bacbdc8e285828bcd1cd9c6cbbc0
Taiji Kondo <kondo@sailboat.co.jp>
2021-10-15 16:18:12 +0900

    ADD: カートが空の場合は空であることを表示

--- src/components/product/CartList.tsx
+++ src/components/product/CartList.tsx
@@ -8,18 +8,24 @@ export const CartList: VFC<Partial<CartListPropsType>> = memo(({ showAddButton =
   const [carts, { addCart, removeCart }] = useSelectProducts()
 
   return (
-    <ul>
+    <>
       {
-        carts.map((cart) => {
-          return (
-            <li key={cart.id}>
-              <p>{cart.name} x{cart.selectedCount}</p>
-              {showAddButton && <button type='button' onClick={() => addCart(cart)}>ADD</button>}
-              {showRemoveButton && <button type='button' onClick={() => removeCart(cart)}>REMOVE</button>}
-            </li>
-          )
-        })
+        carts.length ?
+          <ul>
+            {
+              carts.map((cart) => {
+                return (
+                  <li key={cart.id}>
+                    <p>{cart.name} x{cart.selectedCount}</p>
+                    {showAddButton && <button type='button' onClick={() => addCart(cart)}>ADD</button>}
+                    {showRemoveButton && <button type='button' onClick={() => removeCart(cart)}>REMOVE</button>}
+                  </li>
+                )
+              })
+            }
+          </ul>
+          : <p>Cart is Empty</p>
       }
-    </ul>
+    </>
   )
 })
