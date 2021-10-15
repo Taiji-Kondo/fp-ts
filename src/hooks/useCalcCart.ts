754c1c9bdcfa8085d3f5ff2c4112613a0d0d20ec
Taiji Kondo <kondo@sailboat.co.jp>
2021-10-15 16:58:31 +0900

    REFACTOR: カートの合計金額を求めるロジックをFP化

--- src/hooks/useCalcCart.ts
+++ src/hooks/useCalcCart.ts
@@ -1,11 +1,19 @@
 import { useRecoilValue } from "recoil";
 import {cartsState} from "../stores/atoms/cartState";
+import {map, reduce} from "fp-ts/lib/Array";
+import {ProductType} from "../types/product/ProductType";
+import {pipe} from "fp-ts/function";
 
 export const useCalcCart = () => {
   const cartState = useRecoilValue(cartsState)
 
-  const cartPriceList = cartState.map((cart) => cart.price * (cart.selectedCount ?? 1))
-  const result = cartPriceList.reduce((prev, current) => prev + current, 0)
+  const _cartPriceList = map((cart: ProductType) => cart.price * (cart.selectedCount ?? 1))
+  const _sumPrice = reduce(0, (prev: number, current: number) => prev + current)
 
-  return [result]
+  const result = (products: ProductType[]) => pipe(
+    _cartPriceList(products),
+    _sumPrice
+  )
+
+  return [result(cartState)]
 }
