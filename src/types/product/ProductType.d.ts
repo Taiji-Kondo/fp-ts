0dda98e7767a72388636137a55e0fa720885e43b
Taiji-Kondo <k.taiji1108@gmail.com>
2021-10-16 21:01:34 +0900

    ADD: ProductTypeにdetailを追加

--- src/types/product/ProductType.d.ts
+++ src/types/product/ProductType.d.ts
@@ -1,6 +1,9 @@
+import {ProductDetailType} from "./ProductDetailType";
+
 export type ProductType = {
   id: number
   name: string
   price: number
   selectedCount?: number
+  detail: ProductDetailType
 }
