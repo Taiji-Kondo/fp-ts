136c5c6b3ac4eff53d8e1d54057ce5b988d39b0a
Taiji-Kondo <k.taiji1108@gmail.com>
2021-10-17 17:12:07 +0900

    ADD: 商品に詳細情報を追加

--- src/constants/Products.ts
+++ src/constants/Products.ts
@@ -3,17 +3,29 @@ import {ProductType} from "../types/product/ProductType";
 const APPLE: ProductType = {
   id: 1,
   name: 'apple',
-  price: 100
+  price: 100,
+  detail: {
+    description: 'An Apple',
+    evaluation: 4
+  },
 }
 const ORANGE: ProductType = {
   id: 2,
   name: 'orange',
-  price: 80
+  price: 80,
+  detail: {
+    description: 'An Orange',
+    evaluation: 3.5
+  },
 }
 const BANANA: ProductType = {
   id: 3,
   name: 'banana',
-  price: 200
+  price: 200,
+  detail: {
+    description: 'Two Banana',
+    evaluation: 1.2
+  },
 }
 
 export const PRODUCTS = [APPLE, ORANGE, BANANA]
